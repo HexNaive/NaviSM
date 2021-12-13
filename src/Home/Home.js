5d43c6d195519cd43ff4578c48e4f6e25f1462b2
YuzeTT <61106448+YuzeTT@users.noreply.github.com>
2021-12-13 22:48:30 +0800

    增加谷歌统计

---
+++ src/Home/Home.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import "./Home.css";
+
+function Home() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light"
+        }
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <div className="Home">
+      <ThemeProvider theme={theme}>
+        <div>
+          <Stack
+            sx={{ width: "90%" }}
+            spacing={2}
+            style={{ margin: "0 auto", paddingTop: "20px" }}
+          >
+            <Alert severity="error">
+              <AlertTitle>无法访问</AlertTitle>
+              网站正在开发中，若您为网站管理员，请从 <strong>
+                管理员后台
+              </strong>{" "}
+              进入。
+            </Alert>
+          </Stack>
+        </div>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,9 +11,9 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
