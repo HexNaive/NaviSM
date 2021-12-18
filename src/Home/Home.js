51b8c510c77944be3d7bc1e54dbb83e2b805f733
YuzeTT <ziv1024@163.com>
2021-12-18 19:17:01 +0800

    新增主页

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,49 +1,207 @@
 import * as React from "react";
-import Alert from "@mui/material/Alert";
-import AlertTitle from "@mui/material/AlertTitle";
-import Stack from "@mui/material/Stack";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import ArrowRight from '@mui/icons-material/ArrowRight';
+import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
+import IconHome from '@mui/icons-material/Home';
+import Settings from '@mui/icons-material/Settings';
+import People from '@mui/icons-material/People';
+import PermMedia from '@mui/icons-material/PermMedia';
+import Dns from '@mui/icons-material/Dns';
+import Public from '@mui/icons-material/Public';
+import { useTheme, styled, createTheme, ThemeProvider } from "@mui/material/styles";
+
 // import CssBaseline from "@mui/material/CssBaseline";
 
 import "./Home.css";
 
-function Home() {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+const data = [
+  { icon: <People />, label: '快速开始' },
+  { icon: <Dns />, label: '用户分享' },
+  { icon: <Public />, label: '发布任务' },
+  { icon: <PermMedia />, label: '我的任务' },
+];
 
+const FireNav = styled(List)({
+  '& .MuiListItemButton-root': {
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  '& .MuiListItemIcon-root': {
+    minWidth: 0,
+    marginRight: 16,
+  },
+  '& .MuiSvgIcon-root': {
+    fontSize: 20,
+  },
+});
+
+
+function Home() {
+  const [open, setOpen] = React.useState(true);
   const theme = React.useMemo(
     () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? "dark" : "light"
-        }
-      }),
-    [prefersDarkMode]
-  );
+    createTheme({
+      palette: {
+        // mode: 'dark',
+      },
+    })
+  )
 
   return (
     <div className="Home">
-      <ThemeProvider theme={theme}>
-        <div>
-          <Typography variant="h3" component="div" gutterBottom>
-            <Box sx={{ textAlign: "center", m: 1 }}>NaviSM</Box>
-          </Typography>
-          <Stack
-            sx={{ width: "90%" }}
-            spacing={2}
-            style={{ margin: "0 auto", paddingTop: "20px" }}
-          >
-            <Alert severity="error">
-              <AlertTitle>无法访问</AlertTitle>
-              网站正在开发中，若您为网站管理员，请从 <strong>
-                管理员后台
-              </strong>{" "}
-              进入。
-            </Alert>
-          </Stack>
-        </div>
+      {/* <ThemeProvider theme={theme}> */}
+      <ThemeProvider theme={createTheme({
+          components: {
+            MuiListItemButton: {
+              defaultProps: {
+                disableTouchRipple: true,
+              },
+            },
+          },
+          palette: {
+            mode: 'dark',
+            primary: { main: 'rgb(102, 157, 246)' },
+            background: { paper: 'rgb(5, 30, 52)' },
+          },
+        })}>
+          <div className="center">
+            <Paper elevation={0} sx={{ maxWidth: 256, margin: '0 auto' }}>
+              <FireNav component="nav" disablePadding>
+                <ListItemButton component="a" href="#customized-list">
+                  <ListItemIcon sx={{ fontSize: 20 }}>🏳️‍🌈</ListItemIcon>
+                  <ListItemText
+                    sx={{ my: 0 }}
+                    primary="NaviSM"
+                    primaryTypographyProps={{
+                      fontSize: 20,
+                      fontWeight: 'medium',
+                      letterSpacing: 0,
+                    }}
+                  />
+                </ListItemButton>
+                <Divider />
+                <ListItem component="div" disablePadding>
+                  <ListItemButton sx={{ height: 56 }}>
+                    <ListItemIcon>
+                      <IconHome color="primary" />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary="YuzeTT - 0.5/M/17"
+                      primaryTypographyProps={{
+                        color: 'primary',
+                        fontWeight: 'medium',
+                        variant: 'body2',
+                      }}
+                    />
+                  </ListItemButton>
+                  <Tooltip title="Project Settings">
+                    <IconButton
+                      size="large"
+                      sx={{
+                        '& svg': {
+                          color: 'rgba(255,255,255,0.8)',
+                          transition: '0.2s',
+                          transform: 'translateX(0) rotate(0)',
+                        },
+                        '&:hover, &:focus': {
+                          bgcolor: 'unset',
+                          '& svg:first-of-type': {
+                            transform: 'translateX(-4px) rotate(-20deg)',
+                          },
+                          '& svg:last-of-type': {
+                            right: 0,
+                            opacity: 1,
+                          },
+                        },
+                        '&:after': {
+                          content: '""',
+                          position: 'absolute',
+                          height: '80%',
+                          display: 'block',
+                          left: 0,
+                          width: '1px',
+                          bgcolor: 'divider',
+                        },
+                      }}
+                    >
+                      <Settings />
+                      <ArrowRight sx={{ position: 'absolute', right: 4, opacity: 0 }} />
+                    </IconButton>
+                  </Tooltip>
+                </ListItem>
+                <Divider />
+                <Box
+                  sx={{
+                    bgcolor: open ? 'rgba(71, 98, 130, 0.2)' : null,
+                    pb: open ? 2 : 0,
+                  }}
+                >
+                  <ListItemButton
+                    alignItems="flex-start"
+                    onClick={() => setOpen(!open)}
+                    sx={{
+                      px: 3,
+                      pt: 2.5,
+                      pb: open ? 0 : 2.5,
+                      '&:hover, &:focus': { '& svg': { opacity: open ? 1 : 0 } },
+                    }}
+                  >
+                    <ListItemText
+                      primary="任务"
+                      primaryTypographyProps={{
+                        fontSize: 15,
+                        fontWeight: 'medium',
+                        lineHeight: '20px',
+                        mb: '2px',
+                      }}
+                      secondary="Authentication, Firestore Database, Realtime Database, Storage, Hosting, Functions, and Machine Learning"
+                      secondaryTypographyProps={{
+                        noWrap: true,
+                        fontSize: 12,
+                        lineHeight: '16px',
+                        color: open ? 'rgba(0,0,0,0)' : 'rgba(255,255,255,0.5)',
+                      }}
+                      sx={{ my: 0 }}
+                    />
+                    <KeyboardArrowDown
+                      sx={{
+                        mr: -1,
+                        opacity: 0,
+                        transform: open ? 'rotate(-180deg)' : 'rotate(0)',
+                        transition: '0.2s',
+                      }}
+                    />
+                  </ListItemButton>
+                  {open &&
+                    data.map((item) => (
+                      <ListItemButton
+                        key={item.label}
+                        sx={{ py: 0, minHeight: 32, color: 'rgba(255,255,255,.8)' }}
+                      >
+                        <ListItemIcon sx={{ color: 'inherit' }}>
+                          {item.icon}
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={item.label}
+                          primaryTypographyProps={{ fontSize: 14, fontWeight: 'medium' }}
+                        />
+                      </ListItemButton>
+                    ))}
+                </Box>
+              </FireNav>
+            </Paper>
+          </div>
       </ThemeProvider>
     </div>
   );
